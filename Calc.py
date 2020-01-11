2f9226ea290719a45ca98bdab34b6846f3278d03
Mubarak <salleymubarak.i7@gmail.com>
2020-01-11 08:10:06 +0000

    Added Calculator Class

--- Calc.py
+++ Calc.py
@@ -1,3 +1,22 @@
 """This is a calculator program that helps you do simple mathematical operations"""
 
 print("calculator started".center(40,"*"))
+
+class calc:
+    """Contains all operations needed for the calculator"""
+
+    last_value:float
+    ops = {
+            "+": lambda x,y : x + y ,
+            "-": lambda x,y : x - y ,
+            "*": lambda x,y : x * y ,
+            "/": lambda x,y : x / y ,
+            "**": lambda x,y : x ** y
+        }
+
+    
+    @staticmethod
+    def operate (num1:float, num2:float, operation:str) -> float:
+        last_value = calc.ops[operation](num1,num2)
+        return last_value
+
